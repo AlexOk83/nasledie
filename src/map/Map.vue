20d3962b0b0671f7b4b15e7c36625a428fa57372
Igor Selivestrov <igor456727@yandex.ru>
2020-07-28 23:07:18 +0300

    change icon map

--- src/map/Map.vue
+++ src/map/Map.vue
@@ -39,10 +39,54 @@
                             routingMode: 'masstransit' //— маршрутизация с использованием общественного транспорта. Доступна только для мультимаршрутов (опция multiRoute должна быть выставлена в true);
                             //routingMode: 'auto' автомобильная маршрутизация;
                             //routingMode: 'pedestrian'  — пешеходная маршрутизация. Доступна только для мультимаршрутов (опция multiRoute должна быть выставлена в true);
-                        }
+                        },
+
                     }, {
-                        // Автоматически устанавливать границы карты так,
-                        // чтобы маршрут был виден целиком.
+                        // Внешний вид путевых точек.
+                        wayPointStartIconLayout: "default#image",
+                        wayPointStartIconImageHref: "/src/assets/images/icons/flag_blue.svg",
+                        wayPointStartIconImageSize: [30, 30],
+                        wayPointStartIconImageOffset: [-5, -25],
+                        // Задаем собственную картинку для последней путевой точки.
+                        wayPointFinishIconLayout: "default#image",
+                        wayPointFinishIconImageHref: "/src/assets/images/icons/flag_blue.svg",
+                        wayPointFinishIconImageSize: [30, 30],
+                        wayPointFinishIconImageOffset: [-5, -25],
+
+                        wayPointIconLayout: "default#image",
+                        wayPointIconImageHref: "/src/assets/images/icons/flag_yellow.svg",
+                        wayPointIconImageSize: [30, 30],
+                        wayPointIconImageOffset: [-2, -25],
+                        // Позволяет скрыть иконки путевых точек маршрута.
+                        // wayPointVisible:false,
+
+                        // Внешний вид транзитных точек.
+
+                        viaPointIconLayout: "default#image",
+                        viaPointIconImageHref: "/src/assets/images/icons/flag_blue.svg",
+                        viaPointIconImageSize: [30, 30],
+                        viaPointIconImageOffset: [-5, -25],
+                        viaPointDraggable: false,
+                        // Позволяет скрыть иконки транзитных точек маршрута.
+                        // viaPointVisible:false,
+
+                        // Внешний вид точечных маркеров под путевыми точками.
+                        pinIconLayout: "default#image",
+                        pinIconImageHref: "/src/assets/images/icons/flag_blue.svg",
+                        pinIconImageSize: [30, 30],
+                        pinIconImageOffset: [-5, -25],
+
+                        // Позволяет скрыть точечные маркеры путевых точек.
+                        // pinVisible:false,
+
+                        // Внешний вид линии маршрута.
+                        routeActiveStrokeWidth: [3, 3],
+                        routeActiveStrokeColor: ["#ff0000","#00ff00"],
+
+                        // Внешний вид линии пешеходного маршрута.
+                        routeActivePedestrianSegmentStrokeStyle: "solid",
+                        routeActivePedestrianSegmentStrokeColor: "#00CDCD",
+
                         boundsAutoApply: true
                     });
 
