2dbede1dd8f9e6ba6ad6a6ac938f3dc3ce470715
Igor Selivestrov <igor456727@yandex.ru>
2020-07-31 23:30:54 +0300

    reccomend list

--- src/components/my-route/index.vue
+++ src/components/my-route/index.vue
@@ -1,18 +1,88 @@
 <template>
-    <div class="my-route">
-        Мой {{ data.title }}
+    <div class="myRoute">
+        <div class="myRoute__image">
+            <img :src="data.image" :alt="data.title" >
+            <div class="like" @click="handleAddToMymyRoute" :class="{ 'active': data.isActive }"/>
+        </div>
+        <div class="myRoute__body">
+            <div class="title">{{ data.title }}</div>
+            <div class="text">{{ data.description }}</div>
+
+            <div class="myRoute__params">
+                <div class="way">
+                    <span class="icon " :class="data.wayIcon" />
+                    {{ getWay }}</div>
+                <div class="myRoute__hour">
+                    <span class="icon " :class="data.hoursIcon" />
+                    {{ getHour }}</div>
+                <div class="myRoute__movement">
+                    <div class="item" v-for="item in data.typesOfMovement">
+                        <span class="icon " :class="item.icon" />
+                        <span>{{ item.title }}</span>
+                    </div>
+                </div>
+
+            </div>
+            <div class="points">
+                <span class="label">Точка старта</span>
+                <span class="point-value">{{ data.pointStart.name }}</span>
+
+                <span class="label">Точка назначения</span>
+                <span class="point-value">{{ data.pointEnd.name }}</span>
+            </div>
+            <div class="myRoute__objects">
+                <div class="title__sub">Объекты для посещения</div>
+                <div class="item" v-for="item in data.objects">
+                    <span class="icon icon-object"  />
+                    {{ item.name }}
+
+                </div>
+            </div>
+            <Button  ></Button>
+            <Button  ></Button>
+        </div>
     </div>
 </template>
 
 <script>
+    import Button from "../form-control/Button";
     export default {
-        name: "my-route",
+        name: "my-myRoute",
+        components: {Button},
         props: {
             data: {
                 title: String,
                 description: String,
+                pointStart: {
+                    coordinates: Array,
+                    name: String
+                },
+                pointEnd: {
+                    coordinates: Array,
+                    name: String
+                },
+                typesOfMovement: Array,
+                myRouteTypes: Array,
             }
         },
+        methods: {
+            handleAddToMymyRoute() {
+
+            }
+        },
+        computed: {
+            getWay() {
+                const way = this.data.way / 1000;
+                if (way < 1) {
+                    return `${this.data.way} м`
+                }
+
+                return `${way} км`
+            },
+            getHour() {
+                return `${this.data.hours} часов`
+            }
+        }
     }
 </script>
 
