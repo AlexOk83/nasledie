89da2dd80c7f7939e07b91ba80dcd42eb1d6f722
Alexander Okatyev <a.okatyev@bssys.com>
2020-09-20 00:12:27 +0400

    удалил ненужное

--- src/main.js
+++ src/main.js
@@ -3,26 +3,11 @@ import VueRouter from 'vue-router'
 import store from './store'
 import App from './App.vue'
 import router from './routes'
-import VueResource from 'vue-resource'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.use(VueRouter);
-Vue.use(VueResource);
+
 Vue.use(VueAwesomeSwiper);
-
-Vue.http.options.root = 'http://localhost:3000/';
-
-Vue.http.interceptors.push(request => {
-    request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
-});
-//
-// Vue.config.errorHandler = function(err, vm, info) {
-//     console.log(`Error: ${err.toString()}\nInfo: ${info}`);
-// }
-//
-// Vue.config.warnHandler = function(msg, vm, trace) {
-//     console.log(`Warn: ${msg}\nTrace: ${trace}`);
-// }
 
 new Vue({
     el: '#app',
